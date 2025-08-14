ee3f31e4e879d2787279c38ee02b5e9a2b40c83e
Chad Ridings <ChadRidings@users.noreply.github.com>
2025-08-14 16:40:36 -0700

    Delete api.tsx

--- lib/api/api.tsx
+++
@@ -1,8 +0,0 @@
-const tmdbApiKey = process.env.TMDB_API_KEY;
-
-export const fetchTrendingMovies = async () => {
-    const response = await fetch(
-        `https://api.themoviedb.org/3/trending/movie/week?api_key=${tmdbApiKey}&language=en-US`
-    );
-    return response.json();
-};
